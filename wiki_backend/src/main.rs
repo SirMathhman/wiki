b4e0b6c3331ca1569a54e44e866147af10c30fd1
SirMathhman <SirMathhman@gmail.com>
2023-02-07 17:12:12 -0500

    Clean???

--- wiki_backend/src/main.rs
+++ wiki_backend/src/main.rs
@@ -7,6 +7,14 @@ mod tests {
         title: String,
     }
 
+    impl Clone for Node {
+        fn clone(&self) -> Self {
+            return Node {
+                title: self.title.clone()
+            }
+        }
+    }
+
     impl PartialEq for Node {
         fn eq(&self, other: &Self) -> bool {
             return self.title == other.title;
@@ -16,7 +24,25 @@ mod tests {
     #[test]
     fn test() {
         let first = Node { title: String::from("test") };
-        let second = Node { title: String::from("test") };
+        let node = Node { title: String::from("test") };
+        let second = find_any(State { node }).unwrap();
         assert_eq!(first, second);
     }
+
+    struct State {
+        node: Node,
+    }
+
+    impl State {
+        fn find_any(&self) -> Option<Node> {
+            return Some(self.node.clone());
+        }
+    }
+
+    fn find_any(state: State) -> Option<Node> {
+        return state.find_any();
+    }
+
+    #[test]
+    fn test1() {}
 }
