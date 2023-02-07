c7234618ec7044601f5d537b009ac120b1f64cdf
SirMathhman <SirMathhman@gmail.com>
2023-02-07 16:48:54 -0500

    More dependency stuff

--- wiki_backend/src/main.rs
+++ wiki_backend/src/main.rs
@@ -1,8 +1,8 @@
 use std::env::consts::DLL_PREFIX;
 
 use diesel::{Connection, SqliteConnection};
-use diesel::pg::PgConnection;
 
 fn main() {
     SqliteConnection::establish("./data/data.db").unwrap();
+
 }
