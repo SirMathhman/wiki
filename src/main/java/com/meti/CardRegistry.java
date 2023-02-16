1706795c92588a3c87c7ec2379d35c6a360c6718
SirMathhman <sirmathhman@gmail.com>
2023-02-15 23:06:56 -0500

    Update CardRegistry to avoid identification by titles

--- src/main/java/com/meti/CardRegistry.java
+++ src/main/java/com/meti/CardRegistry.java
@@ -5,26 +5,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CardRegistry {
-    private final Map<String, Card> nodes;
+    private final Map<Integer, Card> nodes;
 
     public CardRegistry() {
         this(Collections.emptyMap());
     }
 
-    public CardRegistry(Map<String, Card> nodes) {
+    public CardRegistry(Map<Integer, Card> nodes) {
         this.nodes = nodes;
     }
 
     public CardRegistry create(Card card) throws RegistryException {
         var title = card.computeTitle();
-        if (nodes.containsKey(title)) {
+        if (hasCardByTitle(title)) {
             var format = "Duplicate card of title '%s' cannot be re-registered.";
             var message = format.formatted(title);
             throw new RegistryException(message);
         }
 
+        var nextKey = nodes.keySet()
+                .stream()
+                .mapToInt(value -> value)
+                .min()
+                .orElse(0);
+
         var copy = new HashMap<>(nodes);
-        copy.put(title, card);
+        copy.put(nextKey, card);
         return new CardRegistry(copy);
+    }
+
+    private boolean hasCardByTitle(String title) {
+        return nodes.values()
+                .stream()
+                .map(Card::computeTitle)
+                .anyMatch(title::equals);
     }
 }
